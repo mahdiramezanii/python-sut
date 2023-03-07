ffbdab534bbcc7bf219efb776a1e65a46e847a48
mahdiramazani <mahdiramazani128@gmail.com>
2023-03-07 11:05:41 +0330

    01

---
+++ tset.py
@@ -0,0 +1,217 @@
+
+#data type
+
+# number=10
+# boll=True
+# float=10.2
+# string="mahdi"
+
+# print(number)
+
+# my_name=input("Enter your name: ")
+
+# print("nmae is: ",my_name)
+
+#===========================================
+# number=2
+# number_2=3
+
+# print(number+number_2)
+
+# print(number*number_2)
+
+# print(number//number_2)
+
+# print(number/number_2)
+# print(number_2**2)
+
+# number_1=int(input("enter number 1: "))
+# number_2=int(input("enter number 2: "))
+
+# print(type(number_1))
+
+# int_number_1=int(number_1)
+# int_number_2=int(number_2)
+
+# print(type(int_number_1))
+# result=number_2-number_1
+
+# print(result)
+
+#String:
+
+# my_string="MAHDI "
+# my_string_1="arman"
+# number=10
+# print(f"number is {number}")
+#print(my_string+" "+my_string_1)
+#print(my_string[2])
+
+# print(my_string.capitalize())
+# print(my_string.find("k"))
+
+# print(my_string.lower())
+# print(my_string_1.upper())
+
+# print(my_string*10)
+
+# print(10>9)
+# print(10==10)
+# print(10!=5)
+
+# print(50/10)
+# print(50//10)
+# print(50%10)
+
+#List in Python:
+
+
+# my_list=["mahdi","10",10,10.2,True,False]
+# my_list.append("Name")
+# print(my_list)
+# print(type(my_list))
+
+# my_list=[10,20,-1,40,0,60]
+
+# item_list=input("enter item list: ")
+
+# my_list.append(item_list)
+
+# print(len(my_list))
+
+# my_list.sort()
+
+# print(my_list)
+
+
+# mytuple = ("apple", "banana", "cherry") 
+
+# print(type(mytuple))
+
+
+#dit in python:
+
+# my_dict={
+#     "name":"mahdi",
+#     "family":"ramazani",
+# }
+
+# thisdict = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964,
+#   "list":[10,20,30]
+# }
+
+# print(thisdict)
+
+#contition in python:
+
+
+# number_1=10
+# number_2=20
+# # print(number_2>number_1)
+
+# if (number_2==number_1):
+#     print("Hello world")
+
+# elif (number_1>number_2):
+
+#     print("number_1 > number_2") 
+
+# elif (number_1 == number_2):
+
+#     print("number_1 < number_2")
+
+# else:
+#     print("Erorr")
+
+# my_lis="Salam man mahdi hastam"
+
+# if "Mahdi"  in my_lis:
+
+#     print(f"hello mahdi")
+
+# else:
+
+#     print("erorr")
+
+
+# my_string="mahdi ramazani"
+
+# search=input("enter item for search: ")
+
+# if search.lower() in my_string:
+
+#     print(f"found: {search}")
+
+# else:
+
+#     print("erorr")
+
+
+# number_1=int(input("enter number_1: "))
+# char=input("Enter + - *: ")
+# number_2=int(input("enter number_2: "))
+
+
+
+# if char=="+":
+
+#     print(f"{number_1} + {number_2} = {number_1+number_2}")
+
+# elif (char == "-"):
+#     print(number_1-number_2)
+
+# elif(char=="*"):
+
+#     print(number_1*number_2)
+
+# a=1
+# while (a<20):
+
+#     print(a) #1,2,3
+
+#     if a==10:
+#         break
+
+#     a=a+1
+
+# my_string="mahdi"
+
+# for item in range(10):
+
+#     print(f"{item}: {my_string}")
+
+# my_list=["mahdi","ali","reza","taha"]
+
+# for item in my_list:
+
+#     if item=="reza":
+
+#         for i in range(5):
+#             print("reza")
+
+#     print(item)
+
+# my_dict={
+
+#     "name":[],
+#     "family":"ahmadi",
+#     "age":20,
+#     "city":"sirjan"
+# }
+
+# for key,value in my_dict.items():
+
+#     print(key,":",value)
+
+
+# for key in my_dict.keys():
+
+#     print(key)
+
+# for v in my_dict.values():
+
+#     print(v)
+
