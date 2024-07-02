2c0985b2427f823e49f1d1c12538bd16b0e26eb9
mahdiramezani-dev <mahdiramazanii@yahoo.com>
2024-07-02 10:56:45 +0330

    add session 2

---
+++ python1403/code/02.py
@@ -0,0 +1,24 @@
+#data type
+#input output
+#define varible
+#opertaion in varible
+#operation on int
+#opertaion on float
+#operation on string  -> sum,format string,index of string
+#operation on bollean
+#Type conversion  (int -> float) , ( float -> int ) , (int -> str ) , (str -> int )
+#Type Hint in Python
+#if statement
+#data structure
+#List
+#Dictionary
+
+
+
+
+
+
+
+
+
+
